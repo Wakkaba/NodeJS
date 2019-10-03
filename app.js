92ceea56e67e33f260e14b2eb127c970d74b312e
Arthur <macbookpro@MacBooks-MacBook-Pro.local>
2019-10-03 14:27:49 +0300

    arrow func and indentation

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
+
 const {StudMake} = require('./wow');
+
+const {fotki} = require('./pht')
+
 // Зробити функцію, яка буде робити профайли стуентів по групах.
 // Створити папка jscx-1800 та jscx-2000
 fs.mkdir(`./jscx-1800`, err => {
@@ -13,8 +17,9 @@ console.log('OK')
 // В конжній папці має бути ще одна пака з імям студента а-ля Viktor Okten. 
 //В той файл потрібно записати інфу про студента. ЙОго данні. Довільні. 
 StudMake('jscx-1800', 'Arthur', 'Age: 22');
+
 StudMake('jscx-2000', 'Andy', 'Age: 42');
 
 // Також на компі мають знаходитьсь фотки. 
 //ФОто потрібно скопіювати в папку з профіайлом студента стрімами.
-createPhoto('./jscx-1800/Arthur/photo.jpg', '../photo/meme.png');
+fotki('./jscx-1800/Arthur/photo.jpg', '../node/jscx-1800/Arthur/meme.png');
--- pht.js
+++ pht.js
@@ -1,11 +1,12 @@
 const fs = require ('fs');
+
 // Також на компі мають знаходитьсь фотки. 
 //ФОто потрібно скопіювати в папку з профіайлом студента стрімами.
 
-function fotki(photoShot1, photoOut2){
+exports.fotki = (photoShot1, photoOut2) => {
+    
     fs.createReadStream(photoOut2).pipe(fs.createWriteStream(photoShot1));
     console.log('Smile');
 
 }
 
-module.exports = fotki;
